52347284cdd4d61291c8cbad7fe6b2940a45811b
Your Name <you@example.com>
2022-01-23 21:38:47 +0900

    new project menu design

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const title = document.getElementById('title');
+const pjts = document.getElementById('toyprojecticon');
+const pjtContainer = document.getElementById('projectContainer');
+const pjtClose = document.getElementById('pjtClose');
 const info = document.getElementById('infoIcon');
 const infoCard = document.getElementById('infoCard');
 const infoclose = infoCard.querySelector('div');
 
-info.addEventListener('click',function(){
-    infoCard.setAttribute('class', 'display');
-    title.removeAttribute('class');
+function changeContent(content) {
+    let cDisplay = document.getElementsByClassName('display');
+    for (let i = 0; i < cDisplay.length; i++) {
+        console.log(cDisplay[i]);
+        cDisplay[i].removeAttribute('class', 'display');
+    };
+    content.setAttribute('class', 'display');
+};
+
+pjts.addEventListener('click', function(){
+    changeContent(pjtContainer);
 });
-infoclose.addEventListener('click',function(){
-    infoCard.removeAttribute('class');
-    title.setAttribute('class', 'display');
+pjtClose.addEventListener('click', function(){
+    changeContent(title);
+})
+info.addEventListener('click', function(){
+    changeContent(infoCard);
+});
+infoclose.addEventListener('click', function(){
+    changeContent(title);
 });
